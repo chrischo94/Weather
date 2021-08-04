ff007c3c99e100690f023d17528d063f62b5bda9
chrischo94 <82658603+chrischo94@users.noreply.github.com>
2021-08-04 13:26:02 +0900

    index.js

--- script.js
+++ script.js
@@ -2,11 +2,53 @@ const searchInput = document.querySelector('#search-input')
 var searchInputVal = document.querySelector('#search-input').value
 const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='
 const apiKey = '&appid=6d7fe1885740c345686c5b56cbcda38f'
+
+let today = moment().format('1')
+let todayTemp = document.querySelector(".temp");
+let todayHumid = document.querySelector(".hum");
+let todayWind = document.querySelector(".wind");
+
+
+let monday = document.getElementById("mon").children[0];
+let mon = moment().add(1, 'days').calendar("l");
+let monTemp = document.querySelector(".monTemp");
+let monHumid = document.querySelector(".monHum");
+let monWind = document.querySelector(".monWind");
+
+
+let tuesday = document.getElementById("tue").children[0];
+let tue = moment().add(1, 'days').calendar("l");
+let tueTemp = document.querySelector(".tueTemp");
+let tueHumid = document.querySelector(".tueHum");
+let tueWind = document.querySelector(".tueWind");
+
+
+let wednesday = document.getElementById("wed").children[0];
+let wed = moment().add(1, 'days').calendar("l");
+let wedTemp = document.querySelector(".wedTemp");
+let wedHumid = document.querySelector(".wedHum");
+let wedWind = document.querySelector(".wedWind");
+
+
+let thursday = document.getElementById("thur").children[0];
+let thur = moment().add(1, 'days').calendar("l");
+let thurTemp = document.querySelector(".thurTemp");
+let thurHumid = document.querySelector(".thurHum");
+let thurWind = document.querySelector(".thurWind");
+
+
+let friday = document.getElementById("fri").children[0];
+let fri = moment().add(1, 'days').calendar("l");
+let friTemp = document.querySelector(".friTemp");
+let friHumid = document.querySelector(".friHum");
+let friWind = document.querySelector(".friWind");
+
+
 const weatherNow = [];
 function getLatLong(searchInput) {
     var latLong = 'https://api.openweathermap.org/geo/1.0/direct?q=' + searchInput + apiKey;
     fetch(latLong)
-        .then(function (data){
+        .then(function (data) {
             return data.json()
         })
         .then(function (data) {
@@ -15,15 +57,14 @@ function getLatLong(searchInput) {
             getWeather(lat, long)
         })
 }
-function getWeather(lat, long){
+function getWeather(lat, long) {
     var url = baseUrl + lat + long + apiKey;
     fetch(url)
-        .then(function (res){
+        .then(function (res) {
             return res.json();
         })
-        .then(function (data){
+        .then(function (data) {
             var weather = {
-                city: data.timezone,
                 icon: data.current.weather[0].icon,
                 temp: data.current.temp,
                 hum: data.current.humidity,
@@ -34,18 +75,25 @@ function getWeather(lat, long){
         }
         )
 }
-function showWeather(){
+function showWeather() {
+    let tempString = data.current.temp.toString();
+    todayTemp.textContent = "Temp: " + tempString + " Fahrenheit";
 
+    let windString = data.current.wind_speed.toString();
+    todayWind.textContent = "Wind: " + windString + " MPH";
+
+    let humidString = data.current.humidity.toString();
+    todayHumid.textContent = "Humidity: " + humidString + " %";
 }
-function handleSearchSubmit(event){
+function handleSearchSubmit(event) {
     event.preventDefault();
-    searchInput= document.querySelector('.search-input').value;
-    
+    searchInput = document.querySelector('.search-input').value;
+
 
     if (!searchInputVal) {
-      
-      return;
-    }  
+
+        return;
+    }
     getWeather();
-  }
-  searchBtn.addEventListener('click', handleSearchFormSubmit);
+}
+searchBtn.addEventListener('click', handleSearchFormSubmit);
