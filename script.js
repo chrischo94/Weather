fc672de733039c4dba49e37d407ef7da882b4e2e
chrischo94 <82658603+chrischo94@users.noreply.github.com>
2021-08-04 13:08:14 +0900

    index.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const searchInput = document.querySelector('#search-input').value;
+const searchInput = document.querySelector('#search-input')
+var searchInputVal = document.querySelector('#search-input').value
 const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='
 const apiKey = '&appid=6d7fe1885740c345686c5b56cbcda38f'
-
+const weatherNow = [];
 function getLatLong(searchInput) {
     var latLong = 'https://api.openweathermap.org/geo/1.0/direct?q=' + searchInput + apiKey;
     fetch(latLong)
@@ -17,8 +18,34 @@ function getLatLong(searchInput) {
 function getWeather(lat, long){
     var url = baseUrl + lat + long + apiKey;
     fetch(url)
-        .then(function (data){
-            return response.json();
+        .then(function (res){
+            return res.json();
         })
-        .then(function ())
+        .then(function (data){
+            var weather = {
+                city: data.timezone,
+                icon: data.current.weather[0].icon,
+                temp: data.current.temp,
+                hum: data.current.humidity,
+                wind: data.current.wind_speed
+            }
+            weatherNow.push(weather)
+
+        }
+        )
 }
+function showWeather(){
+
+}
+function handleSearchSubmit(event){
+    event.preventDefault();
+    searchInput= document.querySelector('.search-input').value;
+    
+
+    if (!searchInputVal) {
+      
+      return;
+    }  
+    getWeather();
+  }
+  searchBtn.addEventListener('click', handleSearchFormSubmit);
